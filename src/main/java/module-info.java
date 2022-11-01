856287f16c9a9d04ec3666497b1cbc8fa555e2af
TovlisKatsi <tovliskatsi@gmail.com>
2022-11-01 19:51:35 +0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.javafx2dengine.javafx2dengine {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.almasb.fxgl.all;
+
+    opens com.javafx2dengine.javafx2dengine to javafx.fxml;
+    exports com.javafx2dengine.javafx2dengine;
+}
